3e06175bad279f2a2f5855defa3cafc69758a0d9
Marco Schmalz <marco@schess.ch>
2019-11-17 22:15:23 +0100

    feat: use jwt for authentication

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,8 +2,9 @@
   <div>
     <span v-if="loggedIn">{{username}} <button @click="logout">Logout</button></span>
     <span v-else>
-      username: <input type="text" placeholder="Username" v-model="loginformUsername"><br/>
-      password: <input type="password" placeholder="Password" v-model="loginformPassword">  <button @click="login">Login</button>
+      username: <input type="text" placeholder="Username" v-model="loginform.username"><br/>
+      password: <input type="password" placeholder="Password" v-model="loginform.password">
+      <button @click="login">Login</button>
     </span>
   </div>
 </template>
@@ -11,43 +12,77 @@
 <script>
 import axios from 'axios'
 
+let RENEW_TIMEOUT = 10 * 60 * 1000   // ten minutes
+
 export default {
   name: 'Login',
-  async created() {
-    try {
-      let response = await axios.get('/api/login/')
-      this.loggedIn = true
-      this.username = response.data.user
-
-    } catch (err) {
-      // Nothing todo
+  data() {
+    return {
+      loggedIn: false,
+      username: '',
+      polling: null,
+      loginform: {},
     }
   },
+  async created() {
+    await this.renew()
+    this.polling = setInterval(this.renew, RENEW_TIMEOUT)
+  },
+  beforeDestroy () {
+    clearInterval(this.polling)
+  },
   methods: {
-    logout() {
-      this.loggedIn  = false
+    async renew() {
+      let token = localStorage.getItem('token')
+      if (token) {
+        try {
+          let response = await axios.post('/api/renew_token/', {token})
+          let new_token = response.data.token
+          localStorage.setItem('token', new_token)
+          axios.defaults.headers.common['Authorization'] = `Bearer ${new_token}`
+
+          let payload = JSON.parse(atob(new_token.split('.')[1]))
+          this.username = payload.user
+          this.loggedIn = true
+          return  // We're good
+        } catch (err) {
+          // Handle error case below
+        }
+      }
+      this.loggedIn = false
+      this.username = ''
+      localStorage.removeItem('token')
     },
+
     async login() {
       try {
-        let response = await axios.post('/api/login/', {username: this.loginformUsername, password: this.loginformPassword})
+        let response = await axios.post('/api/login/', this.loginform)
         if (response.status === 200) {
           this.loggedIn = true;
-          this.username = response.data.user
+          this.loginform = {}
+          let token = response.data.token
+          // Extract payload
+          let payload = token.split('.')[1]
+          // Convert back to 'normal' base64
+          payload = payload.replace(/-/g, '+').replace(/_/g, '/')
+          // Decode and parse
+          payload = JSON.parse(atob(payload))
+          this.username = payload.user
+          localStorage.setItem('token', token)
+          axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
         } else {
           alert('Invalid username or password')
         }
       } catch (error) {
         //nothing
       }
-    }
-  },
-  data() {
-    return {
-      loggedIn: false,
-      username: '',
-      loginformUsername: '',
-      loginformPassword: '',
-    }
+    },
+
+    logout() {
+      this.loggedIn = false
+      this.username = ''
+      localStorage.removeItem('token')
+    },
   },
 }
 </script>
