206cff2ce8e972e63bdff0512f11115613c6b5fd
Marco Schmalz <marco@schess.ch>
2019-11-20 13:19:10 +0100

    feat: routes and stub for statistics page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Music from '../views/Music.vue'
 import Jingles from '../views/Jingles.vue'
+import Stats from '../views/Stats.vue'
 import NotFound from '../views/NotFound.vue'
 
 
@@ -22,6 +23,19 @@ const routes = [
     path: '/jingles',
     name: 'jingles',
     component: Jingles,
+  },
+  {
+    path: '/stats',
+    redirect: () => {
+      let now = new Date()
+      let month = `${now.getFullYear()}-${now.getMonth() + 1}`
+      return { name: 'stats', params: { month } }
+    },
+  },
+  {
+    path: '/stats/:month(\\d{4}-\\d{2})',
+    name: 'stats',
+    component: Stats,
   },
   {
     path: '*',
---
+++ src/views/Stats.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Statistics for {{month}}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },
+  computed:{
+    month() {
+      return this.$route.params.month
+    }
+  },
+  async created() {
+  },
+  beforeDestroy () {
+  },
+  methods: {
+  },
+}
+</script>
+
