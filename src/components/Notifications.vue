12dea94514e318de13281c8390513777c5beb4c7
Marco Schmalz <marco@schess.ch>
2019-11-27 11:24:00 +0100

    revert: remove test code for multiple notifications

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -1,28 +1,15 @@
 <template>
-<div>
   <v-snackbar
     v-model="show"
     :color="color"
     :timeout="4000"
     top
   >
-    1: {{text}}
+    {{text}}
     <v-btn dark text v-on:click="show = false">
         <v-icon>mdi-close</v-icon>
       </v-btn>
   </v-snackbar>
-  <v-snackbar
-    v-model="show2"
-    :color="color"
-    :timeout="4000"
-    top
-  >
-    2: {{text}}
-    <v-btn dark text v-on:click="show = false">
-        <v-icon>mdi-close</v-icon>
-      </v-btn>
-  </v-snackbar>
-  </div>
 </template>
 
 <script>
@@ -42,7 +29,6 @@ export default {
   data() {
     return {
       show: false,
-      show2: false,
       text: '',
       color: '',
     }
@@ -55,12 +41,10 @@ export default {
   },
   watch: {
     notifications() {
-      // console.log(newVal, oldVal)
       if (this.notifications.length > 0) {
         this.color = this.notifications[0].type
         this.text = this.notifications[0].message
         this.show = true
-        this.show2 = true
 
         this.popNotification()
       }
