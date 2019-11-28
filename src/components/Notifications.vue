b7d17cd813c32687bc2410e44f1ec3e9352b6153
Marco Schmalz <marco@schess.ch>
2019-11-28 16:18:17 +0100

    feat: show many notifications at once

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -1,15 +1,22 @@
 <template>
-  <v-snackbar
-    v-model="show"
-    :color="color"
-    :timeout="4000"
-    top
-  >
-    {{text}}
-    <v-btn dark text v-on:click="show = false">
-        <v-icon>mdi-close</v-icon>
-      </v-btn>
-  </v-snackbar>
+  <div>
+    <v-snackbar
+      v-for="(entry, index) in entries"
+      :key="index"
+      v-model="entry.show"
+      :color="entry.color"
+      :timeout="8000"
+      bottom
+      left
+      :style="{bottom: (8 + index * 64) + 'px'}"
+      @input="update"
+    >
+      {{entry.text}}
+      <v-btn dark text v-on:click="hide(entry)">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
@@ -18,19 +25,9 @@ import {mapGetters, mapMutations} from 'vuex'
 
 export default {
   name: 'Notifications',
-
-  created() {
-  },
-
-  beforeDestroy () {
-    clearInterval(this.polling)
-  },
-
   data() {
     return {
-      show: false,
-      text: '',
-      color: '',
+      entries: [],
     }
   },
   computed: {
@@ -38,13 +35,32 @@ export default {
   },
   methods: {
     ...mapMutations(['popNotification']),
+    hide(entry) {
+      entry.show = false
+      this.$forceUpdate()
+    },
+    update() {
+      this.$forceUpdate()
+    },
   },
   watch: {
     notifications() {
       if (this.notifications.length > 0) {
-        this.color = this.notifications[0].type
-        this.text = this.notifications[0].message
-        this.show = true
+        // Find first empty slot
+        for (var entry of this.entries) {
+          if (!entry.show) {
+            break
+          }
+        }
+        // `entries` empty, or full
+        if (!entry || entry.show) {
+          entry = {}
+          this.entries.push(entry)
+        }
+        entry.color = this.notifications[0].type
+        entry.text = this.notifications[0].message
+        entry.show = true
+        this.$forceUpdate()
 
         this.popNotification()
       }
@@ -52,7 +68,3 @@ export default {
   },
 }
 </script>
-
-<style>
-
-</style>
