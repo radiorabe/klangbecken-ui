e595575c64928fd6bcfcb69fadd394ef97e67b95
Marco Schmalz <marco@schess.ch>
2019-12-03 14:03:53 +0100

    Add playlist

--- src/playlists.js
+++ src/playlists.js
@@ -3,6 +3,10 @@ export default {
     name: 'Musik',
     description: 'Hauptplayliste für das Klangbecken',
   },
+  heavy: {
+    name: 'Heavy Rotation',
+    description: 'Dies ist ein Test',
+  },
   jingles: {
     name: 'Jingles',
     description: 'Jingles werden viermal pro Stunde in die Playliste eingefügt. Jeweils zur Minute 05, 20, 35 und 50.',
