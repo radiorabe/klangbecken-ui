0e57b8f0a9ce0cd8f5977d1f62e6cd7feb717f16
Marco Schmalz <marco@schess.ch>
2019-12-04 19:46:09 +0100

    feat: add classics playlist

--- src/playlists.js
+++ src/playlists.js
@@ -3,6 +3,10 @@ export default {
     name: 'Musik',
     description: 'Hauptplayliste für das Klangbecken',
   },
+  classics: {
+    name: 'Classics',
+    description: 'Die Evergreens aus zwei Dekaden RaBe',
+  },
   jingles: {
     name: 'Jingles',
     description: 'Jingles werden viermal pro Stunde in die Playliste eingefügt. Jeweils zur Minute 05, 20, 35 und 50.',
