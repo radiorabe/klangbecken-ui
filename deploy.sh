ba9594ace02830c0ba2978c289c5282ec5d12755
Marco Schmalz <marco@schess.ch>
2020-04-22 16:10:29 +0200

    (chore) Deployment script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# copy files
+scp -r dist/index.html dist/favicon.ico dist/js dist/css dist/fonts dist/img root@vm-0016.vm-admin.int.rabe.ch:/var/www/html/
