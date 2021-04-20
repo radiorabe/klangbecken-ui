12aea10796652c7a897cbab0ba0908c53f9cb6c5
Marco Schmalz <marco@schess.ch>
2021-04-20 22:35:18 +0200

    feat: prompt page reload on API update

---
+++ src/components/NeedReload.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      :value="show"
+      persistent
+      max-width="500"
+    >
+      <v-card>
+        <v-card-title class="headline">Bitte Seite neu laden</v-card-title>
+        <v-card-text>
+          Für die Applikation wurde ein Update eingespielt. Bitte die Seite neu laden, um die Änderungen zu laden.
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="secondary" @click="reload">Neu laden</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "NeedReload",
+  props: ["show"],
+  methods: {
+    reload() {
+        window.location.reload()
+    },
+  },
+}
+</script>
