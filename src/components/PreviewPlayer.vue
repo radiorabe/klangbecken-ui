4ab4a96818138962e691f723a26aa3f9c4698b83
Marco Schmalz <marco@schess.ch>
2021-10-12 21:31:45 +0200

    refactor(preview): code simplification

--- src/components/PreviewPlayer.vue
+++ src/components/PreviewPlayer.vue
@@ -1,13 +1,13 @@
 <template>
   <v-snackbar :value="isPreviewing" bottom multi-line :timeout="-1">
-    <v-container fluid class="flex-grow-1 d-flex justify-space-between align-center flex-nowrap px-0 py-2" style="margin-right: -12px; max-width:100%;">
-      <audio :src="previewPath" v-on:ended="ended" autoplay controls class="flex-grow-0 flex-shrink-0"></audio>
+    <v-container fluid class="flex-grow-1 d-flex justify-space-between align-center flex-nowrap px-0 py-2 ml-3 mr-n2" style="max-width:100%;">
+      <audio :src="previewPath" v-on:ended="clearPreview" autoplay controls class="flex-grow-0 flex-shrink-0"></audio>
       <div class="flex-grow-1 flex-shrink-1 text-truncate" style="min-width: 180px;">
-        <strong>{{preview.title || preview.original_filename}}</strong>
+        <strong>{{previewTitle}}</strong>
         <br/>
-        von <em>{{preview.artist}}</em>
+        von <em>{{previewArtist}}</em>
       </div>
-      <v-btn dark text v-on:click="ended" class="flex-grow-0 flex-shrink-0 px-0 mx-0">
+      <v-btn dark small text v-on:click="clearPreview" class="flex-grow-0 flex-shrink-0 px-0 mx-0">
         <v-icon>mdi-close</v-icon>
       </v-btn>
     </v-container>
@@ -19,18 +19,12 @@ import { mapGetters, mapMutations } from "vuex";
 
 export default {
   name: "PreviewPlayer",
-  data() {
-    return {};
-  },
   computed: {
-    ...mapGetters(["preview", "previewPath", "isPreviewing"])
+    ...mapGetters(["isPreviewing", "previewPath", "previewArtist", "previewTitle"]),
   },
   methods: {
     ...mapMutations(["clearPreview"]),
-    ended() {
-      this.clearPreview();
-    }
-  }
+  },
 };
 </script>
 
--- src/store/preview.js
+++ src/store/preview.js
@@ -1,29 +1,21 @@
 export default {
   state: {
-    preview: {},
-    isPreviewing: false,
+    preview: undefined,
   },
 
   getters: {
-    preview: (state) => state.preview,
-    isPreviewing: (state) => state.isPreviewing,
-    previewPath: (state) => {
-      if (state.isPreviewing) {
-        return `/data/${state.preview.playlist}/${state.preview.id}.${state.preview.ext}`;
-      } else {
-        return "";
-      }
-    },
+    isPreviewing: state => state.preview !== undefined,
+    previewPath: state => state.preview ? `/data/${state.preview.playlist}/${state.preview.id}.${state.preview.ext}` : "",
+    previewTitle: state => state.preview ? state.preview.title || state.preview.original_filename : "",
+    previewArtist: state => state.preview ? state.preview.artist || "<Unbekannter Artist>" : "",
   },
 
   mutations: {
     setPreview: (state, preview) => {
       state.preview = preview;
-      state.isPreviewing = true;
     },
     clearPreview: (state) => {
-      state.preview = {};
-      state.isPreviewing = false;
+      state.preview = undefined;
     },
   },
 
