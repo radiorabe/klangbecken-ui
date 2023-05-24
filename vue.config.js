18cbe23ff15a145fee531128edf8abdbd5793607
Marco Schmalz <marco@schess.ch>
2023-05-24 22:44:59 +0200

    chore: fix node proxy settings

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       "": {
-        target: "http://localhost:5000",
+        target: "http://127.0.0.1:5000",
         changeOrigin: true
       }
     }
