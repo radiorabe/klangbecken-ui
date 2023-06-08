d43df9f55c10a9459951e41638f7eed56d092e8e
Marco Schmalz <marco@schess.ch>
2023-06-08 19:37:47 +0200

    chore: only proxy /api and /data
    ... and not the devserver's /ws websocket endpoint

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,13 @@ module.exports = {
   productionSourceMap: false,
   devServer: {
     proxy: {
-      "": {
+      "/api": {
         target: "http://127.0.0.1:5000",
-        changeOrigin: true
-      }
-    }
+      },
+      "/data": {
+        target: "http://127.0.0.1:5000",
+      },
+    },
   },
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
 };
